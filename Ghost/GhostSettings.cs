48ff92e37ba005386aabae0cdac8054e70ceffff
KageKirin <kagekirin@gmail.com>
2020-09-24 01:26:18 +0900

    Add static GhostSettings for demo.ghost.io

---
+++ Ghost/GhostSettings.cs
@@ -0,0 +1,9 @@
+namespace Ghost
+{
+public sealed class GhostSettings
+{
+    public static string SiteLocation { get; } = "https://demo.ghost.io";
+    public static string RestApiLocation { get; } = "https://demo.ghost.io/ghost/api/v3/content";
+    public static string RestApiKey { get; } = "22444f78447824223cefc48062";
+}
+}
