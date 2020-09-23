20bdd684a9d229a110c3b850606114e2122293fc
KageKirin <kagekirin@gmail.com>
2020-09-24 01:34:19 +0900

    Add data model class for Ghost Tag

---
+++ Ghost/Model/Tag.cs
@@ -0,0 +1,26 @@
+
+namespace Ghost
+{
+public class Tag
+{
+    public string Id {get;set;}
+    public string Name {get;set;}
+    public string Slug {get;set;}
+    public string Description {get;set;}
+    public string FeatureImage {get;set;}
+    public string Visibility {get;set;}
+    public string MetaTitle {get;set;}
+    public string MetaDescription {get;set;}
+    public string OgImage {get;set;}
+    public string OgTitle {get;set;}
+    public string OgDescription {get;set;}
+    public string TwitterImage {get;set;}
+    public string TwitterTitle {get;set;}
+    public string TwitterDescription {get;set;}
+    public string CodeinjectionHead {get;set;}
+    public string CodeinjectionFoot {get;set;}
+    public string CanonicalUrl {get;set;}
+    public string AccentColor {get;set;}
+    public string Url {get;set;}
+}
+}
