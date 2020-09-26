ff97d7f25571fdd5f53fdcca647b14ffaa46a575
KageKirin <kagekirin@gmail.com>
2020-09-26 14:07:09 +0900

    Add data model class for Ghost Author

---
+++ Ghost/Model/Author.cs
@@ -0,0 +1,20 @@
+
+namespace Ghost
+{
+public class Author
+{
+    public string Slug {get;set;}
+    public string Id {get;set;}
+    public string Name {get;set;}
+    public string ProfileImage {get;set;}
+    public string CoverImage {get;set;}
+    public string Bio {get;set;}
+    public string Website {get;set;}
+    public string Location {get;set;}
+    public string Facebook {get;set;}
+    public string Twitter {get;set;}
+    public string MetaTitle {get;set;}
+    public string MetaDescription {get;set;}
+    public string Url {get;set;}
+}
+}
